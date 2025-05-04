745132f0b4539c3e84cb325b2977dd5f24d31d30
Sandeep <sandeepbm886@gmail.com>
2025-05-04 21:39:55 +0530

    Added Signin

--- src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function Page() {
   return (
     <div className="flex min-h-screen w-full items-center justify-center p-4" style={{ height: '100vh' }}>
-      
+      <SignIn />
     </div>
   )
 }
